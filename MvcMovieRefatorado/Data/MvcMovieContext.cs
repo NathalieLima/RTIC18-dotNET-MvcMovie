da456cab4370bf90c3543c5638a96777a1680980
NathalieLima <nathalielima3@gmail.com>
2024-02-29 10:06:59 -0300

    Subindo projeto refatorado

---
+++ MvcMovieRefatorado/Data/MvcMovieContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+        public DbSet<MvcMovie.Models.Studio> Studio { get; set; } = default!;
+        public DbSet<MvcMovie.Models.Artist> Artist { get; set; } = default!;
+    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Entity<Studio>()
+            //     .HasMany(e => e.Movies)
+            //     .WithOne(e => e.Studio)
+            //     .HasForeignKey(e => e.StudioId);
+        }
+
+    }
+}
